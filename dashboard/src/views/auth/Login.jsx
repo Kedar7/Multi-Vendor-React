0088f99df902713c552174ade2ca961371c0cb4c
Kedar7 <kedar.kulkarni010@gmail.com>
2024-12-05 20:44:32 +0530

    MVO6: added images and login view page

--- dashboard/src/views/auth/Login.jsx
+++ dashboard/src/views/auth/Login.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 
 const Login = () => {
+  const [state, setState] = useState({
+    email : '',
+    password :''
+  });
+
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state)
+  };
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-[#ffffff] p-2">
@@ -13,13 +29,15 @@ const Login = () => {
             Please Sign In your account
           </p>
 
-          <form>
+          <form onSubmit={submit}>
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="email">Email</label>
               <input
+                onChange = {inputHandle}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="text"
                 name="email"
+                value = {state.email}
                 placeholder="Email"
                 id="email"
                 required
@@ -29,24 +47,26 @@ const Login = () => {
             <div className="flex flex-col w-full gap-1 mb-3">
               <label htmlFor="password">Password</label>
               <input
+                onChange = {inputHandle}
                 className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
                 type="password"
                 name="password"
+                value = {state.password}
                 placeholder="Password"
                 id="password"
                 required
               />
             </div>
 
-            <button className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-            Sign In
+            <button type="submit"  className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
+              Sign In
             </button>
 
             <div className="flex items-center mb-3 gap-3 justify-center">
               <p>
                 Don't Have an account ?{" "}
                 <Link className="font-bold" to="/register">
-                Sign Up
+                  Sign Up
                 </Link>{" "}
               </p>
             </div>
