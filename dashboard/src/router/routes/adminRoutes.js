d51c3d4222a5722aefcc27bf673d622ea52f7d7d
Kedar7 <kedar.kulkarni010@gmail.com>
2024-12-10 15:44:52 +0530

    MV-14 : added Pagination for orders table and routes for Order Dashboard

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -1,9 +1,14 @@
 import { lazy } from "react";   
-const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'))   
+const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'));
+const OrderDashboard = lazy(() => import("../../views/admin/Orders"))
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
         element : <AdminDashboard/>,
+        role : 'admin'
+    },{
+        path: 'admin/dashboard/orders',
+        element : <OrderDashboard/>,
         role : 'admin'
     }
 ]
