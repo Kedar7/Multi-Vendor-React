10426b2f3ce82b4d9f9ea166c48d41fe2bb50921
Kedar7 <kedar.kulkarni010@gmail.com>
2024-12-11 19:17:54 +0530

    MV-15 : Added Payment request page for seller

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -2,7 +2,8 @@ import { lazy } from "react";
 const AdminDashboard = lazy(()=> import('../../views/admin/AdminDashboard'));
 const OrderDashboard = lazy(() => import("../../views/admin/Orders"));
 const CategoryDashboard = lazy(()=> import('../../views/admin/Category'));
-
+const SellerDashboard =  lazy(() => import('../../views/admin/Seller'));
+const RequestPaymentDashboard =  lazy (()=> import('../../views/admin/RequestPayment'));
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
@@ -16,6 +17,16 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/category',
         element : <CategoryDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/sellers',
+        element : <SellerDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: '/admin/dashboard/payment-request',
+        element : <RequestPaymentDashboard/>,
         role : 'admin'
     }
 ]
