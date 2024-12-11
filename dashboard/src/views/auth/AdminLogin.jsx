d415195e630d976f6623222db043b510481b31f2
Kedar7 <kedar.kulkarni010@gmail.com>
2024-12-11 18:51:31 +0530

    MV-15 : Auth Reducer updated according to category page

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,52 +1,56 @@
-import React, { useState , useEffect} from 'react'; 
-import {  useSelector } from 'react-redux';
-// import { admin_login } from '../../store/Reducers/authReducer';
-import { PropagateLoader } from 'react-spinners';
-import toast from 'react-hot-toast';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import admin_login from "../../store/Reducers/authReducer";
+import messageClear from "../../store/Reducers/authReducer";
+import { PropagateLoader } from "react-spinners";
+import toast from "react-hot-toast";
 import { useNavigate } from 'react-router-dom';
 
 const AdminLogin = () => {
-  // const dispatch = useDispatch();
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { loader, successMessage, errorMessage } = useSelector((state) => state.auth);
-  
+
+  const { loader, errorMessage, successMessage } = useSelector(
+    (state) => state.auth
+  );
 
   const [state, setState] = useState({
     email: "",
-    password: ""
+    password: "",
   });
 
   const inputHandle = (e) => {
     setState({
       ...state,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const submit = (e) => {
     e.preventDefault();
-    // dispatch(admin_login(state));
+    dispatch(admin_login(state));
   };
   const overrideStyle = {
-    display : 'flex',
-    margin : '0 auto',
-    height: '24px',
-    justifyContent : 'center',
-    alignItem : 'center'
-}
+    display: "flex",
+    margin: "0 auto",
+    height: "24px",
+    justifyContent: "center",
+    alignItem: "center",
+  };
 
-useEffect(() => {
-  if (errorMessage) {
-      toast.error(errorMessage)
-      // dispatch(messageClear())
-  }
-  if(successMessage){
-    toast.success(successMessage);
-     //dispatch(messageClear())
-     navigate('/');
+  useEffect(() => {
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+      navigate("/");
+    }
+  }, [dispatch, navigate,errorMessage,successMessage]);
 
-  }
-},[errorMessage,successMessage])
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-[#ffffff] p-2">
@@ -56,7 +60,7 @@ useEffect(() => {
               <img
                 className="w-full h-full"
                 src="http://localhost:3000/images/logo.png"
-                alt="image"
+                alt="admin-image"
               />
             </div>
           </div>
@@ -90,10 +94,15 @@ useEffect(() => {
               />
             </div>
 
-            <button disabled={loader ? true : false}  className='bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3'>
-            {
-               loader ? <PropagateLoader color='#fff' cssOverride={overrideStyle} /> : 'Login'
-            } 
+            <button
+              disabled={loader ? true : false}
+              className="bg-slate-800 w-full hover:shadow-blue-300/ hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
+            >
+              {loader ? (
+                <PropagateLoader color="#fff" cssOverride={overrideStyle} />
+              ) : (
+                "Login"
+              )}
             </button>
           </form>
 
