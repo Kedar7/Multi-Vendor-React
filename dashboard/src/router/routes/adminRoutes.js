a680deed29fd50a5e50c6570e3c0ad50e8681b55
Kedar7 <kedar.kulkarni010@gmail.com>
2024-12-12 09:46:58 +0530

    MV16 : Added Seller Details and Deactive sellers page

--- dashboard/src/router/routes/adminRoutes.js
+++ dashboard/src/router/routes/adminRoutes.js
@@ -4,6 +4,9 @@ const OrderDashboard = lazy(() => import("../../views/admin/Orders"));
 const CategoryDashboard = lazy(()=> import('../../views/admin/Category'));
 const SellerDashboard =  lazy(() => import('../../views/admin/Seller'));
 const RequestPaymentDashboard =  lazy (()=> import('../../views/admin/RequestPayment'));
+const DeactivateSellersDashboard = lazy (() => import('../../views/admin/DeactivateSellers'));
+const SellerRequestDashboarad = lazy(() => import('../../views/admin/SellerRequest'));
+const SellerDetailsDashboard = lazy(() => import("../../views/admin/SellerDetails"))
 export const adminRoutes = [
     {
         path: 'admin/dashboard',
@@ -27,6 +30,21 @@ export const adminRoutes = [
     {
         path: '/admin/dashboard/payment-request',
         element : <RequestPaymentDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: '/admin/dashboard/deactive-sellers',
+        element : <DeactivateSellersDashboard/>,
+        role : 'admin'
+    },
+    {
+        path: '/admin/dashboard/sellers-request',
+        element : <SellerRequestDashboarad/>,
+        role : 'admin'
+    },
+    {
+        path: '/admin/dashboard/seller/details/:sellerId',
+        element : <SellerDetailsDashboard/>,
         role : 'admin'
     }
 ]
